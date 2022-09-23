c7b2a6f7707e94da8be273bda0722b6c8e2c8c74
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 02:26:01 +0100

    Create file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_array - prints n elements of an array of integers
+*@a: integral input
+*@n: integral input
+*/
+void print_array(int *a, int n)
+{
+int m;
+
+a[n];
+for (m = 0; m < n; m++)
+printf("a[m], ");
+}
