98fd2709181e6bc58c549c5758f91392a0490b96
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 21:31:30 +0100

    Update file

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -17,7 +17,7 @@ for (i = 0; i < 98 - 1; i++)
 s1[i] = '*';
 }
 s1[i] = '\0';
-printf ("%s\n", s1);
+printf("%s\n", s1);
 ptr = _strncpy(s1, "First, solve the problem. Then, write the code \n",5);
 printf("%s\n", s1);
 printf("%s\n", ptr);
@@ -36,6 +36,6 @@ printf("\n");
 }
 printf("0x%02x", s1[i]);
 }
-printf ("\n");
+printf("\n");
 return (0);
 }
