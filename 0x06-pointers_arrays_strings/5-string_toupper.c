f80266969a8c8e27053bb6e46c447c5ce819bba0
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 23:03:58 +0100

    Create file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**string_toupper - changes all lowercase letters of a string to uppercase
+*@a: pointer input
+*Return: pointer
+*/
+char *string_toupper(char *)
+{
+char *a;
+int n;
+
+for (n = 0; a[n] != '\0'; n++;)
+{
+if (a[n] >= 'a' && a[n] <= 'z')
+{
+a[n] = a[n] - 32;
+}
+}
+a[n] = '\0';
+return (a);
+}
