886747071798adfbdc3f04f3f1e7d31b6d04a725
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 03:52:48 +0100

    Create file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+**_strncat - concatenates two strings
+*@dest: pointer input
+*@src: pointer input
+*@n: integral input
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int m, o;
+
+dest[o] = 0;
+for (m = 0; m < n && src[m] != 0; m++)
+dest[o + n] = '\0';
+return (dest);
+}
