b134ad226e0398fc0c3134d5c19a7de0f9b49718
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 01:03:01 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*swap_int - swaps the values of two integers
+*@a: integral input
+*@b: integral input
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+
+*a = temp;
+*a = *b;
+*b = temp;
+}
