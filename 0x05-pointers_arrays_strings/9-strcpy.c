9857283871a575ca7686d6f492f9694f1904e304
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 02:54:22 +0100

    Create file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+include "main.h"
+
+/**
+**_strcpy - copies contents of array a pointer points to to another pointer
+*dest: pointer input
+*src: pointer input
+*Return: value of dest pointer
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+dest[i] = src[i];
+i++;
+dest[i] = '\0';
+}
