fd9e764eaeab9800138e05bf0c6f78a15e3a28da
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 02:10:53 +0100

    Update file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,15 +11,15 @@ int n;
 int l;
 int length_of_the_string;
 l = length_of_the_string;
-if (l % 2 = 0)
+if (l % 2 == 0)
 {
 n = l / 2;
 }
-else if (l % 2 = 1)
+else if (l % 2 == 1)
 {
 n = (l - 1) / 2;
 }
 printf("%c", str[n++]);
 n++;
-printf('\n');
+printf("\n");
 }
