107e925ab58072d24ff6fa0a9deeed51a6c8f742
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 23:55:05 +0100

    Create file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: pointer input
+*Return: length of a string
+*/
+int _strlen(char *s)
+{
+int l;
+
+for (l = 0; s[l] != '\0'; l++)
+return (l);
+}
