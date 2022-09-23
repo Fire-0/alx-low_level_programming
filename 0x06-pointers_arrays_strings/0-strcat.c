6b21a06eb281a75e5cd2a3cd90e7429a2a9c1f61
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 03:40:58 +0100

    Create file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+**_strcat - contacenates two strings
+*@dest: pointer input
+*@src: pointer input
+*Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int r;
+
+dest[] += src[r];
+Return (dest);
+}
