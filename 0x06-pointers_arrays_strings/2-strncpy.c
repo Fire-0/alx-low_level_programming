5cfffb9b6b4512e925c86ff0b39c26937b853aa9
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-23 20:34:40 +0100

    Create file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**_strncpy - copies a string
+*@dest: pointer input
+*@src: pointer input
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int m;
+
+for (m = 0; m < n && src[m] != '\0'; m++)
+{
+src[m] = dest[m];
+}
+return (dest);
+}
