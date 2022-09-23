f608b8638c04ed6c8ad56613b0b3f7950535efeb
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-24 00:57:20 +0100

    Create file

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts2 - prints every other character of a string, followed by a new line
+*@str: pointer input
+*/
+void puts2(char *str)
+{
+int n = 0;
+for (n = 0; str[n] != '\0'; n++)
+{
+printf("%c\n", str[n])
+}
+}
