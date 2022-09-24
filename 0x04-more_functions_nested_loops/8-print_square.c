9ff24639e7a017c2946b959761bc4dea20057b2e
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-24 03:24:24 +0100

    Update file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_square - prints a square, followed by a new line
+*@size: integral input
+*/
+void print_square(int size)
+{
+int n = 0;
+
+for (n = 0; n == size; n++)
+{
+for (size = 1; size == n; size++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size == 0)
+{
+_putchar('\n');
+}
+}
