b173bfa18146fc7591864cc140858a1ccb3b5452
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-24 02:51:23 +0100

    Create file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*main - prints numbers 1 - 100 followed by a new line
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+n = 1;
+for (n = 1; n <= 100; n++)
+if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+if (n % 5 == 0)
+{
+printf("Buzz ");
+if (n = 100)
+{
+printf("Buzz");
+}
+}
+if (n % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else
+}
+printf("%d ", n);
+}
+Return (0);
+}
