6c139e2715f3870b852917fbddbbfe0daa9a0ed1
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-26 19:31:37 +0100

    create file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strpbrk - searches a string for any set of a byte
+*@s: main string
+*@accept: substring
+*Return: pointer to first occurrence of any set of byte of accept in s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int n = 0;
+
+while (*s != *accept)
+{
+if (*s != *accept)
+{
+return();
+}
+else
+{
+return(s);
+}
+}
