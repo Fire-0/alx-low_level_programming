196a1632fa5c1bd9287bd196ed16ade80284c1c8
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-26 09:39:23 +0100

    Create file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_memset - fills memory with a constant byte
+*@s: pointer input
+*@b: constant byte input
+*@n: bytes of memory area
+*Return: pointer to s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+s[i] = 'b';
+_putchar(s[i]);
+return (s);
+}
