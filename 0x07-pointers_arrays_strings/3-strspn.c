bf00c35ff80daecb31e5ec48a6b4ef431bd88a28
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-26 17:35:29 +0100

    Create file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strspn - gets the length of a prefix substring
+*@s: main string
+*@accept: substring
+*Return: length of substring
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int m, n = 0;
+
+for (m = 0; m < n, m++)
+{
+if (s[n] == accept[m])
+{
+return (m);
+}
+}
+}
