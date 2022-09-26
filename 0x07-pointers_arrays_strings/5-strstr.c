055928d020830aba73f06ed9ad60c787e8f61390
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-26 19:45:30 +0100

    Create file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strstr - function that locates a substring
+*@haystack: main string
+*@needle: substring
+*Return: pointer to first occurrence of substring in main string
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int n;
+
+for (n = O; haystack[n] != *needle; n++)
+{
+*haystack != '\0';
+if (haystack[n] != *needle)
+{
+return ('\O');
+}
+else
+{
+return (haystack);
+}
+}
+}
