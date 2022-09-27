04355b1862f7e4feb8582295ca0e9d1935d58667
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-27 19:36:12 +0100

    Create file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_chessboard - prints chessboard
+*@a[8]: array input
+*Return: nothing
+*/
+void print_chessboard(char (*a)[8])
+{
+int m, n;
+
+for (m = 0; m < 8; m++)
+{
+for (n = 0; n < 8; n++)
+{
+_putchar(a[m][n]);
+}
+_putchar('\n');
+}
+}
