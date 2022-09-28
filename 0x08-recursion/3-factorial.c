dec1debc3c687ba4fd9190667db0fc05dc25f911
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-28 11:31:50 +0100

    Create file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*factorial - calculates the factorial of a number
+*@n: integral input
+*Return: factorial of integer
+*/
+int factorial (int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+factorial(n) = n * factorial(n - 1);
+}
