57c011959ef06720630e8e4e8df58b106c883946
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-28 20:36:42 +0100

    Update file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,7 @@ int _strlen_recursion(char *s)
 {
 int n;
 
-if (s[n] == '\0')
+if (*s == '\0')
 {
-return (0);
-}
-return (1 + _strlen_recursion(s[n - 1]));
+return (1 + _strlen_recursion(s - 1));
 }
