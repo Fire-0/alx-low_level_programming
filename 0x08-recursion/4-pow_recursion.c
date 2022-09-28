99cc7e0f415ed4ac03c03ea59c96bcd5907e8d88
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-28 11:53:35 +0100

    Update file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_pow_recursion - calculates the power of a number to another number
+*@x: integral input
+*@y: power input
+*Return: value of x raised to the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+if (y = 0)
+{
+return (1);
+}
+else if (y < 0)
+{
+return (-1);
+}
+return (n * n ^ (y - 1));
+}
