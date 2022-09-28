1ec945fb18171a16f3b4d4839e5b17dd1bb78223
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-28 21:12:16 +0100

    Create file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*is_palindrome - determines if a string is a palindrome
+*@s: string input
+*Return: 1 if string is a palindrome or is empty, 0 if otherwise
+*/
+int is_palindrome(char *s)
+{
+if (*s == '\0')
+{
+return (1);
+}
+else
+{
+if (*s != _print_rev_recursion(s))
+{
+return (0);
+}
+is_palindrome(s) = 1 * (is_palindrome(s + 1)
+return (1);
+}
+}
