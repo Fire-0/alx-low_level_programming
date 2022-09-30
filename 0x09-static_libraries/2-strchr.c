5fd1cd8b5a3030ae9fe1f7e16901a0644eb1c426
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 10:17:03 +0100

    Create file

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strchr - finds the first occurrence of a character in a string
+*@s: string input
+*@c: character input
+*Return: pointer to first occurrence of c
+*/
+char *_strchr(char *s, char c)
+{
+int n;
+
+for (n = 0; s[n] != c; n++)
+if (s[n] == c)
+{
+return (s);
+}
+return ('\0');
+}
