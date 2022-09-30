29c97791fd13a49095a41c369eb24d3bcb427e08
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 10:18:14 +0100

    Create file

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strspn - gets the length of a prefix substring
+*@s: main string
+*@accept: substring
+*Return: length of substring
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int m, n = 0;
+
+for (m = 0; *s == *accept; m++)
+if (s[n] != accept[m])
+{
+return (m);
+}
+return ('\0');
+}
