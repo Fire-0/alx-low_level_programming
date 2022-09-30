5df86bc1edfb7c76e91db2bbfe49a6c4939d95e8
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 13:35:07 +0100

    Create file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - prints the sum of positive numbers
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int add, n;
+
+for ( n = 1; n < argc; n++)
+{
+add += argv[n];
+printf("%d\n", add);
+if (argc < 2)
+{
+printf("0");
+}
+if (!(argv[n] <= 0 && arg[n] >= 0))
+{
+return (1);
+}
+}
+return (0);
+}
