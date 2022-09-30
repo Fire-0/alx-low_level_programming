f55cddab736c988209e8996450052f757cc8ffb8
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 10:14:00 +0100

    Create file

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_memset - fills memory with a constant byte
+*@s: pointer input
+*@b: constant byte input
+*@n: bytes of memory area
+*Return: pointer to s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
