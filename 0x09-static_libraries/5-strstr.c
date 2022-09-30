b92ee69a6ed9fe6a5864d049c28908541affc77e
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 10:20:40 +0100

    Create file

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strstr - function that locates a substring
+*@haystack: main string
+*@needle: substring
+*Return: pointer to first occurrence of substring in main string
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int n;
+
+for (n = 0; haystack[n] != *needle; n++)
+if (haystack[n] == *needle)
+{
+return (haystack);
+}
+return ('\0');
+}
