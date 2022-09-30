0984842fd8166ebfd6c948cc339f665fe29f5c25
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 12:33:06 +0100

    Create file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*main - a program that prints it's name, followed by a new line
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+_putchar(argv[O]);
+_putchar('\n');
+}
