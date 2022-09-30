5bf28d18dacfa79eecd3750d7b2dca6936287f96
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 10:26:15 +0100

    Create file

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strcmp - compares two strings
+*@s1: pointer input
+*@s2: pointer input
+*Return: difference between first mismatched elements of s1 and s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+int n = 0, l = 0;
+
+for (; s1[n] == s2[n]; n++)
+l = s1[n] - s2[n];
+if (l != 0)
+{
+return (l);
+}
+else
+{
+return (0);
+}
+}
