fa92febb56ee0f6c5010ac1eb0f9ac1608b8074f
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 11:21:04 +0100

    Create file

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs-Function that computes the absolute value of an integer 
+ * x: integral input 
+ * Return:Absolute value of x
+ */
+int _abs(int x)
+{	
+if (x < 0)
+	{
+		x *= -1;
+	}
+	return (x);
+}
