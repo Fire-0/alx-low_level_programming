9f4322b44eb5b720acf7666b3c133020530d4d10
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-09-30 13:10:37 +0100

    Create file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - prints all arguments entered
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int n;
+
+for (n = 0; n < argc; n++)
+printf("%s\n", argv[n];
+return (0);
+}
