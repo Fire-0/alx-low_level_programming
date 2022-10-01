677e9e6731a60995df484a26ae04e799254795a7
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-10-01 08:13:02 +0100

    Update file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,10 +2,12 @@
 
 /**
 *main - prints the number of argument
+*@argc: argument count
+*@argv: argument entry
 *Return: 0
 */
 int main(int argc, char __attribute__ ((unused)) *argv[])
 {
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 return (0);
 }
