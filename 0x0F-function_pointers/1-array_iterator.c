d69cb694179124009a11b5b1e1b75f9f465682b7
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-10-12 23:14:17 +0100

    Update file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "functions_pointers.h"
-#include "_putchat.c"
+#include "_putchar.c"
 #include <stdio.h>
 
 /**
@@ -22,5 +22,5 @@ for (n = 0; n < size; n++)
 _putchar(array[n] + '0');
 }
 }
-(*action)(&array[n]);
+action(&array[n]);
 }
