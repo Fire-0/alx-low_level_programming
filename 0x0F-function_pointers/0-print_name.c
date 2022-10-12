2e88f78746894bcf8bd8ac440719504d3fa8fe90
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-10-12 21:13:34 +0100

    Create file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+
+/**
+*print_name - prints a name
+*@name: character pointer
+*@f: function pointer
+*Return: nothing
+*/
+void print_name(char *name, void (*f)(char*))
+{
+unsigned int n;
+
+n = 0;
+while (name[n])
+{
+_putchar(name[n]);
+}
+n++;
+f = &name;
+}
