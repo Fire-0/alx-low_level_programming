f682a80b7b9fb8c49a77af2c3a652cd6beddfc47
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-10-12 22:01:20 +0100

    Create file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+
+/**
+*array_iterator - executes a function
+*function that is given as a parameter on each element of an array
+*@array: array input
+*@size: size of array
+*@action: pointer to function
+*Return: nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int n;
+
+for (n = 0; n < size; n++)
+{
+_putchar(array[n]);
+}
+action(&array[n]);
+}
