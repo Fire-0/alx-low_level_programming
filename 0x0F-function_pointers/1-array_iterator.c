0c24bb46a5aef2a36d87d31eb114d6ed0866e040
Fire-0 <111375482+Fire-0@users.noreply.github.com>
2022-10-12 23:11:59 +0100

    Update file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "functions_pointers.h"
+#include "_putchat.c"
 
 /**
 *array_iterator - executes a function
@@ -12,11 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t n;
 
+array = malloc(sizeof(int) * size);
 if (size != 0)
 {
 for (n = 0; n < size; n++)
 {
-return (array[n]);
+_putchar(array[n] + '0');
 }
 }
 action(&array[n]);
